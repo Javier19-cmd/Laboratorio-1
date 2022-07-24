356645af68a55cc0a360c59bd7621f38f300bff4
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-07-23 19:48:40 -0600

    creando matrices de puntos para las figuras

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from gl import *
 
 #Matriz del primer polígono.
 
-m = [
+figura1 = [
     [165, 380],
     [185, 360],
     [180, 330],
@@ -15,7 +15,7 @@ m = [
     [193, 383],
 ]
 
-# m =[
+# figura11 =[
 #     [165, 380, 0, 0, 0],
 #     [0, 0, 0, 185, 360, 0, 0],
 #     [0, 0, 180, 330, 0, 0, 0],
@@ -28,7 +28,7 @@ m = [
 #     [193, 383, 0, 0, 0, 0, 0],
 #     ] #Matriz de puntos.
 
-# pl1 = [
+# llenado1 = [
 #     [165, 380, 0, 0, 0, 0, 0, 0], #Primera fila y primera columna. (Primer punto de la primera fila)
 #     [185, 360, 0, 0, 0, 0, 0, 0], #Segunda fila y primera columna. (Primer punto de la segunda fila)
 #     [180, 330, 0, 0, 0, 0, 0, 0], #Cuarta fila y primera columna. (Punta de la izquierda abajo)
@@ -40,6 +40,22 @@ m = [
 #     [0, 0, 0, 205, 410, 0, 0, 0], #Punta de hasta arriba de la estrella.
 #     [0, 0, 193, 383, 0, 0, 0, 0], #Primera fila y segunda columna. (Segundo punto de la primera fila)
 # ]
+
+#Matriz del segundo polígono.
+figura2 = [
+    [321, 335],
+    [288, 286],
+    [339, 251],
+    [374, 302]
+]
+
+
+#Matriz del tercer polígono.
+figura3 = [
+    [377, 249],
+    [411, 197],
+    [436, 249]
+]
 
 def main():
 
@@ -73,11 +89,11 @@ def main():
     #             #print("Después del if: ", m[i][j], m[i][j + 1])
     #             glVertex(m[i][j], m[i][j + 1])
 
-    # #Recorriendo la matriz p1.
-    for i in range(len(m)):
+    # #Recorriendo la matriz figura1.
+    for i in range(len(figura1)):
         #Aquí se ponen los puntos en el mapa de bits.
         #print(p1[i][0], p1[i][1])
-        glVertex(m[i][0], m[i][1])
+        glVertex(figura1[i][0], figura1[i][1])
 
     #Haciendo línea entre los puntos.
     line(165, 380, 185, 360) #Inicio de la figura.
@@ -91,13 +107,13 @@ def main():
     line(205, 410, 193, 383)
     line(193, 383, 165, 380) #Final de la figura.
 
-    
-
     #Creando el segundo polígono.
-    glVertex(321, 335)
-    glVertex(288, 286)
-    glVertex(339, 251)
-    glVertex(374, 302)
+
+    # #Recorriendo la matriz figura2.
+    for i in range(len(figura2)):
+        #Aquí se ponen los puntos en el mapa de bits.
+        #print(p1[i][0], p1[i][1])
+        glVertex(figura2[i][0], figura2[i][1])
 
     #Uniendo los puntos.
     line(321, 335, 288, 286) #Inicio de la figura.
@@ -106,14 +122,19 @@ def main():
     line(374, 302, 321, 335) #Final de la figura.
 
     #Creando el tercer polígono.
-    glVertex(377, 249)
-    glVertex(411, 197)
-    glVertex(436, 249)
+        # #Recorriendo la matriz figura2.
+    for i in range(len(figura3)):
+        #Aquí se ponen los puntos en el mapa de bits.
+        #print(p1[i][0], p1[i][1])
+        glVertex(figura3[i][0], figura3[i][1])
 
     #Uniendo los puntos.
     line(377, 249, 411, 197) #Inicio de la figura.
     line(411, 197, 436, 249)
     line(436, 249, 377, 249) #Final de la figura.
+
+
+
 
     glFinish() #Escribiendo la ventana.
 
